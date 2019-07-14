a0f450b98c80007dccc2554cb78207b6379035c2
Darren Wu <junta.wu@easystack.cn>
2019-07-14 08:07:28 +0800

    Implment the deploy_srpm.sh

---
+++ deploy_srpm.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+. common.sh
+
+SRPM=$1
+if [ -z $SRPM ]; then
+    echo "usage: $0 [kernel srpm]"
+    exit 1
+fi
+
+mkdir -p rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS} 2>/dev/null || true
+rpm --define "_topdir $PWD/rpmbuild" -i $SRPM
+# the fedora doesn't have sh-uils, however, we don't need it. skip it.
+sed -i "s/sh-utils, //" ./rpmbuild/SPECS/kernel.spec
+
+docker run -ti --name=aarch64-kernel-build -w /root/rpmbuild \
+-v $PWD/rpmbuild:/root/rpmbuild \
+$DOCKER_IMG \
+rpmbuild -bp --target=$(uname -m) SPECS/kernel.spec
+
